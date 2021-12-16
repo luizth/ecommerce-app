1736e482666b93d448de0e25b4b890863e477408
Luiz Thomasini <luizthomasini@gmail.com>
2021-12-15 22:24:29 -0300

    Adds JWT token generation on login.

--- admin/users/views.py
+++ admin/users/views.py
@@ -1,10 +1,12 @@
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import exceptions
+from rest_framework.views import APIView
 
-from .serializers import UserSerializer
-
+from .authentication import generate_access_token
 from .models import User
+from .serializers import UserSerializer
 
 
 @api_view(['GET'])
@@ -39,4 +41,10 @@ def login(req):
     if not user.check_password(password):
         raise exceptions.AuthenticationFailed('Incorrect password.')
 
-    return Response('success')
+    response = Response('')
+    token = generate_access_token(user)
+    response.set_cookie(key='jwt', value=token, httponly=True)
+    response.data = {
+        'jwt': token
+    }
+    return response
