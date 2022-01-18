c5161e347d745d4f6032c5555b7d4c0cd5dd7761
Luiz Alfredo Thomasini <luizt@TI-02.guiainvest.com>
2022-01-18 17:25:50 -0300

    Creates serializer and api views for Products

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from admin.pagination import CustomPagination
+from users.authentication import JWTAuthentication
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductGenericAPIView(
+    generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = CustomPagination
+
+    def get(self, req, pk=None):
+        if pk:
+            return Response({
+                'data': self.retrieve(req, pk).data
+            })
+
+        return self.list(req)
+
+    def post(self, req):
+        return Response({
+            'data': self.create(req).data
+        })
+
+    def put(self, req, pk=None):
+        return Response({
+            'data': self.partial_update(req, pk).data
+        })
+
+    def delete(self, req, pk=None):
+        return self.destroy(req, pk)
