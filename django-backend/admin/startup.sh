2067420ed5847bfcfe3adba390727ff3a75d0181
Luiz Thomasini <luizthomasini@gmail.com>
2022-01-19 23:17:54 -0300

    Some console style configuration

--- django-backend/admin/startup.sh
+++ django-backend/admin/startup.sh
@@ -7,31 +7,31 @@ NC='\033[0m' # No Color
 echo "${YEL}Migrating: ${NC}UsersMigrations"
 python3 manage.py makemigrations users
 python3 manage.py migrate
-echo "${YEL}Migrated: ${NC}UsersMigrations"
+echo "${GRE}Migrated: ${NC}UsersMigrations"
 
 echo "${YEL}Migrating: ${NC}ProductsMigrations"
 python3 manage.py makemigrations products
 python3 manage.py migrate
-echo "${YEL}Migrated: ${NC}ProductsMigrations"
+echo "${GRE}Migrated: ${NC}ProductsMigrations"
 
 echo "${YEL}Migrating: ${NC}OrdersMigrations"
 python3 manage.py makemigrations orders
 python3 manage.py migrate
-echo "${YEL}Migrated: ${NC}OrdersMigrations"
+echo "${GRE}Migrated: ${NC}OrdersMigrations"
 
 # Fixtures
 echo "${YEL}Seeding: ${NC}PermissionsFixtures"
 python3 manage.py loaddata fixtures/permissions.json
-echo "${YEL}Seeded: ${NC}PermissionsFixtures"
+echo "${GRE}Seeded: ${NC}PermissionsFixtures"
 
 echo "${YEL}Seeding: ${NC}RolesFixtures"
 python3 manage.py loaddata fixtures/roles.json
-echo "${YEL}Seeded: ${NC}RolesFixtures"
+echo "${GRE}Seeded: ${NC}RolesFixtures"
 
 echo "${YEL}Seeding: ${NC}UsersFixtures"
 python3 manage.py loaddata fixtures/users.json
-echo "${YEL}Seeded: ${NC}UsersFixtures"
+echo "${GRE}Seeded: ${NC}UsersFixtures"
 
 echo "${YEL}Seeding: ${NC}OrdersFixtures"
 python3 manage.py loaddata fixtures/orders.json
-echo "${YEL}Seeded: ${NC}OrdersFixtures"
+echo "${GRE}Seeded: ${NC}OrdersFixtures"
